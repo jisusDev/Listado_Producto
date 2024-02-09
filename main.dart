9bb1070ececdc0e8662388ffe4133c2af02ce4f1
jisusDev <156206996+jisusDev@users.noreply.github.com>
2024-02-09 10:47:13 -0500

    Class Product

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+}
+class Product {
+  String name;
+  double price;
+  double discount;
+
+Product( this.name, this.price, this.discount);
+
+}
